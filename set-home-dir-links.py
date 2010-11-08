b648812459864142442d0e065e8ed61121b420a3
Dan Davison <dandavison7@gmail.com>
2010-11-08 12:37:39 +0000

    Expand tilde in symlink script

--- set-home-dir-links.py
+++ set-home-dir-links.py
@@ -4,20 +4,23 @@ import os
 
 targets = {}
 
-targets['.bashrc'] = '~/config/bash/bashrc'
-targets['.dircolors'] = '~/config/dircolors/dircolors'
-targets['.emacs'] = '~/config/emacs/emacs-init.el'
-targets['.emacs_bash'] = '~/config/emacs/emacs_bash'
-targets['.gitconfig'] = '~/config/git/gitconfig'
-targets['.gitignore'] = '~/config/git/gitignore'
-targets['.inputrc'] = '~/config/readline/inputrc'
-targets['.mailcap'] = '~/config/mailcap'
-targets['.mailrc'] = '~/config/email/mailrc'
-targets['.mairixrc'] = '~/config/mairix/mairixrc-gnus'
-targets['.mime-types'] = '~/config/mime.types'
-targets['.procmailrc'] = '~/config/procmailrc'
+targets['~/.bashrc'] = '~/config/bash/bashrc'
+targets['~/.dircolors'] = '~/config/dircolors/dircolors'
+targets['~/.emacs'] = '~/config/emacs/emacs-init.el'
+targets['~/.emacs_bash'] = '~/config/emacs/emacs_bash'
+targets['~/.gitconfig'] = '~/config/git/gitconfig'
+targets['~/.gitignore'] = '~/config/git/gitignore'
+targets['~/.inputrc'] = '~/config/readline/inputrc'
+targets['~/.mailcap'] = '~/config/mailcap'
+targets['~/.mailrc'] = '~/config/email/mailrc'
+targets['~/.mairixrc'] = '~/config/mairix/mairixrc-gnus'
+targets['~/.mime-types'] = '~/config/mime.types'
+targets['~/.procmailrc'] = '~/config/procmailrc'
 
 for link, target in targets.iteritems():
+    link = os.path.expanduser(link)
+    target = os.path.expanduser(target)
+    
     if os.path.exists(link):
         newname = link + '-renamed'
         print '%s moved to %s' % (link, newname) 
