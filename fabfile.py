14b91c3d4b8abe1525d69dc5443c87eb64608cc9
Dan Davison <dandavison7@gmail.com>
2011-08-07 10:31:01 -0700

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,18 +3,27 @@ from fabric.api import local, run, env
 
 env.hosts = ['analysis']
 
-def deploy():
+REPOS = [('~/bin', 'git@github.com:dandavison/bin.git'),
+         ('~/bin/counsyl', 'analysis:bin-counsyl'),
+         ('~/config', 'git@github.com:dandavison/config.git'),
+         ('~/config/bash/bashrc', 'git@github.com:dandavison/bashrc.git'),
+         ('~/config/emacs', 'git@github.com:dandavison/emacs-config.git')]
+
+
+def deploy(counsyl=False):
+    set_links()
     sync_repos()
-    validate_dotfiles()
 
 
 def sync_repos():
-    for d in ['config', 'bin', 'emacs']:
-        local('cd ~/%s && git push' % d)
-        run('cd ~/%s && git pull' % d)
+    for dir, url in REPOS:
+        local('cd %s && git push' % dir)
+        if run('test -d %s' % dir).failed:
+            run('git clone %s %s' % (url, dir))
+        run('cd %s && git pull' % dir)
 
 
-def validate_dotfiles():
+def set_links():
     targets = {}
     targets['~/.bashrc'] = '~/config/bash/bashrc'
     targets['~/.dircolors'] = '~/config/dircolors/dircolors'
@@ -31,10 +40,8 @@ def validate_dotfiles():
     targets['~/.Rprofile'] = '~/config/R/Rprofile.R'
 
     for link, target in targets.iteritems():
-        link = os.path.expanduser(link)
-        target = os.path.expanduser(target)
 
-        if os.path.exists(link):
-            run('rm ' + link)
+        if run('test -f %s' % link).succeeded:
+            run('rm %s' % link)
 
         run('ln -s %s %s' % (target, link))
