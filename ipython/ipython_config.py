86e38dd6cc945c4bc7e288a79b44e2d5a838a7ea
Dan Davison <dan@counsyl.com>
2012-03-21 23:30:10 -0700

    Update ipython config

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,9 +1,14 @@
 c = get_config()
 
-c.TerminalInteractiveShell.colors = 'NoColor'
+# Why doesn't this work?
+# c.TerminalIPythonApp.classic = True
 
-c.TerminalInteractiveShell.prompt_in2 = '... '
-c.TerminalInteractiveShell.prompt_in1 = '>>> '
-c.TerminalInteractiveShell.prompt_out = '    '
+c.PromptManager.in_template =  '>>> '
+c.PromptManager.in2_template = '... '
+c.PromptManager.out_template = '    '
 
 c.TerminalInteractiveShell.banner1 = ''
+
+# (NoColor, Linux, LightBG)
+c.TerminalInteractiveShell.colors = 'NoColor'
+# c.PromptManager.color_scheme = 'LightBG'
