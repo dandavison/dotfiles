7e65fa543c53e0bcf8aa40b15b2cccd0d46e136f
Dan Davison <dan@counsyl.com>
2015-07-31 19:39:15 -0400

    setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
-ln -s git/gitconfig ~/.gitconfig 
-ln -s tmux/tmux.conf ~/.tmux.conf 
+ln -s `readlink -f dircolors/dircolors` ~/.dircolors
+ln -s `readlink -f git/gitconfig` ~/.gitconfig
+ln -s `readlink -f tmux/tmux.conf` ~/.tmux.conf
