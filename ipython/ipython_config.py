8615c381fae71adb5301a7258f1704bcc84c6a8e
Dan Davison <dandavison7@gmail.com>
2016-10-01 17:29:39 -0700

    Delete stuff from ipython config

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,26 +1,4 @@
 c = get_config()
 
-# Why doesn't this work?
-# c.TerminalIPythonApp.classic = True
-
-c.PromptManager.in_template =  '>>> '
-c.PromptManager.in2_template = '... '
-c.PromptManager.out_template = '    '
-
-c.TerminalInteractiveShell.banner1 = ''
-
 # (NoColor, Linux, LightBG)
 c.TerminalInteractiveShell.colors = 'NoColor'
-# c.PromptManager.color_scheme = 'Linux'  # 'LightBG'
-
-c.InteractiveShellApp.extensions = [
-    'autoreload',
-]
-
-c.InteractiveShellApp.exec_lines = [
-    'import sys',
-    'import os',
-    'sys.path.append(os.path.expanduser("~"))',
-]
-
-c.InteractiveShellApp.pylab_import_all = False
