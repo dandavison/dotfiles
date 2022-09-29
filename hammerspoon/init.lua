6b209ac3db46ac3aa52a1b58e9ac02f6bc1535ac
Dan Davison <dandavison7@gmail.com>
2022-09-29 06:52:54 -0400

    hammerspoon vscode projct switch hotkey

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,3 +7,13 @@ hs.hotkey.bind({"cmd"}, "/", function()
     hs.application.launchOrFocus("/Applications/Alacritty.app")
   end
 end)
+
+hs.hotkey.bind({"ctrl"}, "v", function()
+  local alacritty = hs.application.find('alacritty')
+  if not alacritty:isFrontmost() then
+    hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+  -- Send key stroke 'v' followed by Enter to current application
+  hs.eventtap.keyStroke({}, "v", alacritty)
+  hs.eventtap.keyStroke({}, "return", alacritty)
+end)
