8dadc2933d75c410074d47a3456c6f604fe0683a
Dan Davison <dandavison7@gmail.com>
2022-12-18 07:33:44 -0500

    Use Alacritty.release.app

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,14 +4,14 @@ hs.hotkey.bind({"cmd"}, "/", function()
   if alacritty:isFrontmost() then
     alacritty:hide()
   else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
+    hs.application.launchOrFocus("/Applications/Alacritty.release.app")
   end
 end)
 
 hs.hotkey.bind({"ctrl"}, " ", function()
   local alacritty = hs.application.find('alacritty')
   if not alacritty:isFrontmost() then
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
+    hs.application.launchOrFocus("/Applications/Alacritty.release.app")
     hs.eventtap.keyStroke({}, "v", alacritty)
   else
     hs.eventtap.keyStroke({"ctrl"}, " ", alacritty)
