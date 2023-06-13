29055494acc3269d964208e8e2c27521bf2e53eb
Dan Davison <dandavison7@gmail.com>
2023-06-13 13:49:13 -0400

    hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,29 +1,9 @@
--- -- https://github.com/alacritty/alacritty/issues/862#issuecomment-616873890
--- hs.hotkey.bind({"cmd"}, "'", function()
---   local alacritty = hs.application.find('alacritty')
---   if alacritty:isFrontmost() then
---     alacritty:hide()
---   else
---     hs.application.launchOrFocus("/Applications/Alacritty.release.app")
---   end
--- end)
-
-hs.hotkey.bind({"cmd"}, ";", function()
-    local warp = hs.application.find('warp')
-    if warp:isFrontmost() then
-      warp:hide()
+-- https://github.com/alacritty/alacritty/issues/862#issuecomment-616873890
+hs.hotkey.bind({ "cmd" }, "'", function()
+    local alacritty = hs.application.find('alacritty')
+    if alacritty:isFrontmost() then
+        alacritty:hide()
     else
-      hs.application.launchOrFocus("/Applications/Warp.app")
+        hs.application.launchOrFocus("/Applications/Alacritty.app")
     end
 end)
-  
--- hs.hotkey.bind({"ctrl"}, " ", function()
---   local alacritty = hs.application.find('alacritty')
---   if not alacritty:isFrontmost() then
---     hs.application.launchOrFocus("/Applications/Alacritty.release.app")
---     hs.eventtap.keyStroke({}, "v", alacritty)
---   else
---     hs.eventtap.keyStroke({"ctrl"}, " ", alacritty)
---   end
---   hs.eventtap.keyStroke({}, "return", alacritty)
--- end)
