b51730502fd21c59f49493ac05e9add4b794f121
Dan Davison <dandavison7@gmail.com>
2023-07-15 20:43:20 -0400

    🧬

--- setup.sh
+++
@@ -1,3 +0,0 @@
-ln -s `readlink -f dircolors/dircolors` ~/.dircolors
-ln -s `readlink -f git/gitconfig` ~/.gitconfig
-ln -s `readlink -f tmux/tmux.conf` ~/.tmux.conf
--- setup_all.sh
+++
@@ -1,5 +0,0 @@
-cd /tmp
-for repo in config shell-config emacs-config; do
-    git clone "https://github.com/dandavison/$repo.git"
-    sh $repo/setup.sh
-done
