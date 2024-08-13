a1b9d7b4c8bfa22dc50ab7ca0fe12884ef91fa1e
Dan Davison <dandavison7@gmail.com>
2024-08-13 11:13:25 -0400

    hammerspoon bindings, cleanup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,32 +8,35 @@ Logger = hs.logger.new('dan', "debug")
 
 
 -- https://www.hammerspoon.org/docs/hs.keycodes.html#map
-
 -- https://github.com/alacritty/alacritty/issues/862#issuecomment-616873890
-hs.hotkey.bind({ "cmd" }, "'", function()
+
+local function terminal()
     local alacritty = hs.application.find('alacritty')
     if alacritty:isFrontmost() then
         alacritty:hide()
     else
         hs.application.launchOrFocus("/Applications/Alacritty.app")
     end
-end)
+end
 
-hs.hotkey.bind({ "cmd" }, "space", function()
+local function wormholeSelect()
     hs.application.launchOrFocus("/Applications/Wormhole.app")
-end)
+end
 
-hs.hotkey.bind({}, "f13", function()
-    hs.application.launchOrFocus("/Applications/Wormhole.app")
-end)
 
-hs.hotkey.bind({ "cmd", "control" }, "left", function()
+local function wormholePrevious()
     hs.http.get("http://wormhole:7117/previous-project/", nil)
-end)
+end
 
-hs.hotkey.bind({ "cmd", "control" }, "right", function()
-    hs.http.get("http://wormhole:7117/previous-project/", nil)
-end)
+-- terminal
+hs.hotkey.bind({ "cmd" }, "'", terminal)
+hs.hotkey.bind({}, "f16", terminal)
+
+-- wormhole
+hs.hotkey.bind({ "cmd" }, "space", wormholeSelect)
+hs.hotkey.bind({}, "f13", wormholeSelect)
+hs.hotkey.bind({ "cmd", "control" }, "left", wormholePrevious)
+hs.hotkey.bind({ "cmd", "control" }, "right", wormholePrevious)
 
 
 local current_id, threshold
@@ -44,14 +47,14 @@ Swipe:start(3, function(direction, distance, id)
         if distance > threshold then
             threshold = math.huge
             if direction == "left" then
-                hs.http.get("http://wormhole:7117/previous-project/", nil)
+                wormholePrevious()
                 -- hs.notify.new({
                 --     title = "→→→→",
                 --     informativeText = "",
                 --     autoWithdraw = true,
                 -- }):send()
             elseif direction == "right" then
-                hs.http.get("http://wormhole:7117/previous-project/", nil)
+                wormholePrevious()
                 -- hs.notify.new({
                 --     title = "←←←←",
                 --     informativeText = "",
