961bd0bbb655b6fbe7edeb255ff9065a0ef54703
Dan Davison <dandavison7@gmail.com>
2026-07-02 23:00:21 -0400

    nvim config: github_light theme via lazy.nvim, white background

---
+++ nvim/init.lua
@@ -0,0 +1,24 @@
+-- Bootstrap lazy.nvim (auto-clones on first launch).
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone", "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.o.background = "light"
+
+require("lazy").setup({
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("github-theme").setup({})
+      vim.cmd("colorscheme github_light") -- canvas.default #ffffff
+    end,
+  },
+})
